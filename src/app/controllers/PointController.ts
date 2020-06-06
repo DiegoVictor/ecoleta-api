73c740e7a32e3400c9560ef717244baf8c930285
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-06-06 01:02:34 -0300

    Upload images

--- src/app/controllers/PointController.ts
+++ src/app/controllers/PointController.ts
@@ -61,8 +61,7 @@ class PointController {
       longitude,
       city,
       uf,
-      image:
-        'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80',
+      image: request.file.filename,
     };
 
     const [id] = await transaction('points').insert([point]);
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import ItemController from './app/controllers/ItemController';
 import PointController from './app/controllers/PointController';
@@ -7,15 +8,23 @@ import cityUfItemsValidator from './app/validator/cityUfItemsValidator';
 import idValidator from './app/validator/idValidator';
 import pointValidator from './app/validator/pointValidator';
 
+import multer_config from './config/multer';
+
 const point_controller = new PointController();
 const item_controller = new ItemController();
 
 const routes = Router();
+const upload = multer(multer_config);
 
 routes.get('/items', item_controller.index);
 
 routes.get('/points/', cityUfItemsValidator, point_controller.index);
 routes.get('/points/:id', idValidator, point_controller.show);
-routes.post('/points', pointValidator, point_controller.store);
+routes.post(
+  '/points',
+  upload.single('image'),
+  pointValidator,
+  point_controller.store,
+);
 
 export default routes;
