40c4ac2c7911dc4dad9384c2ebb30d4512f083ed
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-12-20 13:00:14 -0300

    Use camel case

--- src/routes.ts
+++ src/routes.ts
@@ -1,30 +1,29 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import ItemController from './app/controllers/ItemController';
-import PointController from './app/controllers/PointController';
-
+import ItemsController from './app/controllers/ItemsController';
+import PointsController from './app/controllers/PointsController';
 import cityUfItemsValidator from './app/validator/cityUfItemsValidator';
 import idValidator from './app/validator/idValidator';
 import pointValidator from './app/validator/pointValidator';
 
-import multer_config from './config/multer';
+import multerConfig from './config/multer';
 
-const point_controller = new PointController();
-const item_controller = new ItemController();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
 const routes = Router();
-const upload = multer(multer_config);
+const upload = multer(multerConfig);
 
-routes.get('/items', item_controller.index);
+routes.get('/items', itemsController.index);
 
-routes.get('/points/', cityUfItemsValidator, point_controller.index);
-routes.get('/points/:id', idValidator, point_controller.show);
+routes.get('/points/', cityUfItemsValidator, pointsController.index);
+routes.get('/points/:id', idValidator, pointsController.show);
 routes.post(
   '/points',
   upload.single('image'),
   pointValidator,
-  point_controller.store,
+  pointsController.store,
 );
 
 export default routes;
